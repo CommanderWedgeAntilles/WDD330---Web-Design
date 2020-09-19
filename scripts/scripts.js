1bad7d4cc689e98e1be8552825468c5bae44f0e6
CommanderWedgeAntilles <fli12003@byui.edu>
2020-09-19 15:12:19 -0600

    Index core

---
+++ scripts/scripts.js
@@ -0,0 +1,11 @@
+//EventListeners
+document.querySelector('#menu').addEventListener('click',toggleMenu);
+/*function declaration*/
+// function toggleMenu(){
+
+// }
+
+/*function expression*/
+// const toggle = function() {
+
+// }
