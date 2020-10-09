67b63a84dd3ac04b8430858421f244fa66fa2a66
CommanderWedgeAntilles <fli12003@byui.edu>
2020-10-08 18:11:23 -0600

    Week05 initial

---
+++ week05/team/script/script.js
@@ -0,0 +1,13 @@
+const player = {
+    name: ['Player 1', 'Player 2'],
+    turn: { yes: true, no: false}
+};
+function game(){
+
+}
+function turn(){
+
+}
+function addXorO(cell){
+    if(player.name == 'Player 1'&& player.turn==)cell.innerHTML("X")
+}
