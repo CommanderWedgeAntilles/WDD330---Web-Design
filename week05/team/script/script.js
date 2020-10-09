bbb1be95df15db0f148307c1f770ecc40bdcaf6e
CommanderWedgeAntilles <fli12003@byui.edu>
2020-10-08 18:20:15 -0600

    improved javaScript

--- week05/team/script/script.js
+++ week05/team/script/script.js
@@ -8,6 +8,22 @@ function game(){
 function turn(){
 
 }
+
+
+divNames = ['topLeft', 'topMid','topRight','midLeft','midMid','midRight','bottomLeft', 'bottomMid', 'bottomRight'];
+ 
+for(var i =0; i < 9; i++){
+    document.getElementById(divNames[i]).addEventListener('click', clickDiv(divNames[i]));
+}
+function clickDiv(val) {
+    if(document.getElementById(val).value == 'X' || document.getElementById(val).value == 'O'){
+        alert("This Space is Occupied.");
+    }else{
+        addXorO(val);
+    }
+}
+        
 function addXorO(cell){
-    if(player.name == 'Player 1'&& player.turn==)cell.innerHTML("X")
+    if(player.name == 'Player 1'){document.getElementById(cell).innerHTML('X');}
+    else{document.getElementById(cell).innerHTML('O');}
 }
