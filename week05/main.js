b720aa3f0c961e6616b49f9ac14f473e712da15d
CommanderWedgeAntilles <fli12003@byui.edu>
2020-10-11 01:04:26 -0600

    week05 changes

--- week05/main.js
+++ week05/main.js
@@ -2,3 +2,21 @@ const input = form.elements.searchInput;
 input.addEventListener('focus', () => alert('focused'), false);
 input.addEventListener('blur', () => alert('blurred'), false);
 input.addEventListener('change', () => alert('changed'), false);
+
+const form = document.forms['search'];
+form.addEventListener ('submit', search, false);
+function search(event) {
+    alert(`You Searched for: ${input.value}`);
+    event.preventDefault();
+}
+
+//in order to allow the user to enter and use the search bar do following
+input.addEventListener('focus', function(){
+    if (input.value==='Search Here') {
+        input.value = '' 
+    }
+}, false);
+input.addEventListener('blur', function(){
+    if(input.value === '') {
+        input.value = 'Search Here';
+    } }, false);
