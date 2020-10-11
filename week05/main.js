93b1e1b0e612b6366297eecdac580d358e1ad241
CommanderWedgeAntilles <fli12003@byui.edu>
2020-10-11 02:06:34 -0600

    chANGE

--- week05/main.js
+++ week05/main.js
@@ -1,9 +1,10 @@
+const form = document.forms['search'];
 const input = form.elements.searchinput;
 input.addEventListener('focus', () => alert('focused'), false);
 input.addEventListener('blur', () => alert('blurred'), false);
 input.addEventListener('change', () => alert('changed'), false);
 
-const form = document.forms['search'];
+
 form.addEventListener ('submit', search, false);
 function search(event) {
     alert(`You Searched for: ${input.value}`);
